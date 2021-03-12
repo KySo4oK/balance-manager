ce0be3e95c0afb998237311dc9b633ec954e48a0
KySo4oK <prostorostuk08@gmail.com>
2021-03-12 16:45:45 +0200

    face problem with the same month and different years

--- src/test/java/org/example/BalanceServiceTest.java
+++ src/test/java/org/example/BalanceServiceTest.java
@@ -15,12 +15,23 @@ public class BalanceServiceTest {
     private BalanceService testedInstance = new BalanceServiceImpl();
 
     @Test
-    public void testGetNegativeDaysInMonths() {
+    public void shouldReturnTwoWhenTransactionInOneDay() {
         List<Transaction> transactions = new ArrayList<>();
+        transactions.add(new Transaction(-102, LocalDate.now()));
         transactions.add(new Transaction(-102, LocalDate.now()));
         Account account = new Account(100, transactions);
         Map<Month, Integer> negativeDaysInMonths = testedInstance.getNegativeDaysInMonths(account);
         Assert.assertTrue(negativeDaysInMonths.containsValue(1));
+    }
+
+    @Test
+    public void shouldReturnTwoWhenTransactionInAnotherYear() {
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.add(new Transaction(-102, LocalDate.now()));
+        transactions.add(new Transaction(-102, LocalDate.now().minusYears(1)));
+        Account account = new Account(100, transactions);
+        Map<Month, Integer> negativeDaysInMonths = testedInstance.getNegativeDaysInMonths(account);
+        Assert.assertTrue(negativeDaysInMonths.keySet().size() == 2);
     }
 
     @Test
